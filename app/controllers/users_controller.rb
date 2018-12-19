aff290334d0fa3b3fd9048c1bb1e891af8dff54d
Richard <richie1988@gmail.com>
2018-12-19 10:52:40 -0500

    made Users controller and User model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      redirect_to root_path
+    else
+      p @user.errors.messages
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  # get 'users/new'
+  # get 'users/edit'
+  # get 'users/index'
+  # get 'users/show'
+  resources :users
+  root 'pages#home'
+  get '/signup', to: 'users#new'
+  get '/about', to: 'pages#about'
+  get '/faq', to: 'pages#faq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
